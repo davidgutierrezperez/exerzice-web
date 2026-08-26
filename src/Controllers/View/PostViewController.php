e858ade09891b4142586ff6028e27829947a972a
David Gutierrez <davidgutierrezperez8@gmail.com>
2026-08-26 11:57:50 +0200

    Code documetation has been updated

--- src/Controllers/View/PostViewController.php
+++ src/Controllers/View/PostViewController.php
@@ -3,15 +3,24 @@
 namespace Controllers\View;
 
 use Twig;
-use Override;
 
+/**
+ * The class PostViewController represents a specific view controller to render posts related pages.
+ */
 final class PostViewController extends BaseViewController {
 
-
+    /**
+     * Default constructor of the class PostViewController.
+     * @param Twig\Environment $twig Twig environment.
+     */
     public function __construct(Twig\Environment $twig){
         parent::__construct($twig);
     }
 
+    /**
+     * Renders the page to create a new post.
+     * @return void
+     */
     public function create(): void {
         echo $this->twig->render('pages/posts/create-post.twig');
     }
